883683d546c6703faa2d5d266c9a84143e39666c
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-08-08 14:26:45 +0200

    Test

--- packages/core/integration-tests/test/plugin.js
+++ packages/core/integration-tests/test/plugin.js
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import path from 'path';
-import {bundle, outputFS as fs, distDir} from '@parcel/test-utils';
+import {bundle, outputFS as fs, distDir, overlayFS} from '@parcel/test-utils';
 
 describe('plugin', function() {
   it("continue transformer pipeline on type change that doesn't change the pipeline", async function() {
@@ -23,5 +23,31 @@ parcel-transformer-b`,
     );
 
     assert.deepEqual(fs.readdirSync(distDir), ['index.test']);
+  });
+
+  it('invalidate the cache based on loadConfig in a packager', async function() {
+    let fixture = path.join(__dirname, '/integration/packager-loadConfig');
+    let entry = path.join(fixture, 'index.txt');
+    let config = path.join(fixture, 'foo.config.json');
+    let b = await bundle(entry, {
+      inputFS: overlayFS,
+      disableCache: false,
+    });
+
+    assert.strictEqual(
+      await overlayFS.readFile(b.getBundles()[0].filePath, 'utf8'),
+      '1234',
+    );
+
+    await overlayFS.writeFile(config, JSON.stringify({contents: 'xyz'}));
+
+    b = await bundle(entry, {
+      inputFS: overlayFS,
+      disableCache: false,
+    });
+    assert.strictEqual(
+      await overlayFS.readFile(b.getBundles()[0].filePath, 'utf8'),
+      'xyz',
+    );
   });
 });
